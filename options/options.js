3b913b0fd172b2ac767dbdb4c2c4155cb60d6f3b
Crowdbotics <team@crowdbotics.com>
2023-08-06 11:22:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fitio-42681",
+  url: "https://fitio-42681.botics.co",
+  api: "https://fitio-42681.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
